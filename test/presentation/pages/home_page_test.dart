c0e0414f80b63e6d549887bccdca4aa3596b9ddf
Katya_Sobolevskaya <katyasobolevskaya11@gmail.com>
2023-07-14 13:32:20 +0300

    create test on changing tabs on home page

---
+++ test/presentation/pages/home_page_test.dart
@@ -0,0 +1,35 @@
+import 'package:di_counter/presentation/pages/home/home_controller.dart';
+import 'package:di_counter/presentation/pages/home/home_page.dart';
+import 'package:di_counter/presentation/pages/home/home_tabs.dart';
+import 'package:di_counter/presentation/theme/di_counter_themes.dart';
+import 'package:di_counter/presentation/theme/theme_provider.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:get/get.dart';
+
+void main() {
+  setUp(() {
+    Get.put<HomeController>(HomeController());
+  });
+
+  tearDown(() {
+    Get.delete<HomeController>();
+  });
+
+  testWidgets('Changes tabs after tapping on the icons',
+      (WidgetTester tester) async {
+    await tester.pumpWidget(
+      ThemeProvider(
+        theme: lightTheme,
+        child: GetMaterialApp(
+          home: HomePage(),
+        ),
+      ),
+    );
+
+    for (final tab in HomeTabs.values) {
+      await tester.tap(find.byWidget(tab.icon));
+      await tester.pumpAndSettle();
+      expect(find.byWidget(tab.page), findsOneWidget);
+    }
+  });
+}
